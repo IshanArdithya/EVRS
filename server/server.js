1861a43b7d90c1d381d9dcfac3153d2aa32730e5
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2025-07-11 14:33:17 +0530

    DB Initialized

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+
+// middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("API is running...");
 });
 
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
